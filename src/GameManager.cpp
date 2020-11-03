ef019b25c8d6cf86b6eac76ca576f7455e3d7d24
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-11-03 14:26:09 +0000

    Added a game manager class

---
+++ src/GameManager.cpp
@@ -0,0 +1,21 @@
+#include "GameManager.h"
+
+GameManager::GameManager()
+{
+
+}
+
+GameManager::~GameManager()
+{
+
+}
+
+void GameManager::Update()
+{
+
+}
+
+void GameManager::Draw()
+{
+    
+}
---
+++ src/GameManager.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Car.h"
+
+class GameManager
+{
+public:
+    GameManager();
+    ~GameManager();
+
+    void Update();
+    void Draw();
+};
