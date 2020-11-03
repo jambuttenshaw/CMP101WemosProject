a87227aaaab440adc3c44663bcd0c923981783ca
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-11-03 21:09:32 +0000

    Working on track graphics

--- src/game_objects/Track.cpp
+++ src/game_objects/Track.cpp
@@ -2,7 +2,16 @@
 
 Track::Track()
 {
+    // create the track
+    m_TrackVertices[0] = Point({40, -40, 0});
+    m_TrackVertices[1] = Point({88, -40, 0});
+    m_TrackVertices[2] = Point({40, 104, 0});
+    m_TrackVertices[3] = Point({88, 104, 0});
 
+    m_TrackEdgeIndices[0] = 0;
+    m_TrackEdgeIndices[1] = 2;
+    m_TrackEdgeIndices[2] = 1;
+    m_TrackEdgeIndices[3] = 3;
 }
 
 Track::~Track()
@@ -17,5 +26,14 @@ void Track::Update()
 
 void Track::Draw(Adafruit_SSD1306& display)
 {
-    
+    // the number of lines is half the number of indices
+    int numIndices = sizeof(m_TrackEdgeIndices) / sizeof(unsigned int);
+
+    for (int i = 0; i < numIndices; i += 2)
+    {
+        Point p1 = m_TrackVertices[m_TrackEdgeIndices[i]];
+        Point p2 = m_TrackVertices[m_TrackEdgeIndices[i + 1]];
+        
+        display.drawLine(p1.X(), p1.Y(), p2.X(), p2.Y(), WHITE);
+    }
 }
--- src/game_objects/Track.h
+++ src/game_objects/Track.h
@@ -2,6 +2,11 @@
 
 #include <Adafruit_SSD1306.h>
 
+#include <Geometry.h>
+
+#include <vector>
+
+
 class Track
 {
 public:
@@ -12,5 +17,7 @@ public:
     void Draw(Adafruit_SSD1306& display);
 
 private:
-
+    Point m_TrackVertices[4]; // an array of points that define the shape of the track
+    unsigned int m_TrackEdgeIndices[4]; // an array of indices that define how the points that make up the track should be joined together
+    // unsigned int m_TrackAreaIndices[6]; // an array of indices that define the area that the track covers, for checking when the car is on or off the track
 };
