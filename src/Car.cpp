c29c82c2f767dfa89eb9953b4abb8ac419dd6687
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-11-03 16:43:39 +0000

    Working on car class

--- src/Car.cpp
+++ src/Car.cpp
@@ -1,10 +1,16 @@
 #include "Car.h"
 
 Car::Car()
+    : m_Position(Point()), m_Direction(Rotation())
 {
 
 }
 
+Car::Car(Point position, Rotation direction)
+    : m_Position(position), m_Direction(direction)
+{
+
+}
 
 Car::~Car()
 {
@@ -18,5 +24,18 @@ void Car::Update()
 
 void Car::Draw(Adafruit_SSD1306& display)
 {
-    display.drawLine(0, 0, 63, 32, 1);
+    int posX = (int)m_Position.X();
+    int posY = (int)m_Position.Y();
+
+    Point dir = GetDirectionVector();
+    int endX = posX + m_ViewLineLength * (int)dir.X();
+    int endY = posY + m_ViewLineLength * (int)dir.Y();
+
+    display.drawLine(posX, posY, endX, endY, 1);
+}
+
+Point Car::GetDirectionVector()
+{
+    Point p({ 1, 0, 0 });
+    return m_Direction * p;
 }
--- src/Car.h
+++ src/Car.h
@@ -7,11 +7,26 @@ class Car
 {
 public:
     Car();
+    Car(Point position, Rotation direction);
     ~Car();
+
+    inline void SetPosition(Point position) { m_Position = position; }
+    inline void SetRotation(Rotation rot) { m_Direction = rot; }
+
+    inline Point GetPosition() { return m_Position; }
+    inline Rotation GetRotation() { return m_Direction; }
+    inline Point GetDirection() { return GetDirectionVector(); }
 
     void Update();
     void Draw(Adafruit_SSD1306& display);
 
 private:
+    Point GetDirectionVector();
+private:
+    Point m_Position;
+    // the rotation matrix that would rotate the direction vector (1, 0)
+    // to the direction that the car is facing in
+    Rotation m_Direction;
 
+    int m_ViewLineLength = 16;
 };
