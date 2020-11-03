49c6cbaa8ec871c4c49afaecb459dba4d7821558
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-11-03 20:24:59 +0000

    Updated input.h to add button press

--- src/core/Input.h
+++ src/core/Input.h
@@ -5,5 +5,14 @@
 class Input
 {
 public:
+    static void Init()
+    {
+        pinMode(D6, INPUT_PULLUP);
+    }
+
     static int GetAnalogueIn(int pin = A0) { return analogRead(pin); }
+    
+    static bool GetButtonPress() { GetDigitalRead(D6); }
+
+    static bool GetDigitalRead(int pin) { digitalRead(pin); }
 };
