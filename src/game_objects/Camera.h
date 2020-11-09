0071c79a6bd8db9e68ab80ae957d0a63c4017b06
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-11-09 13:32:42 +0000

    Camera can now be rotated

--- src/game_objects/Camera.h
+++ src/game_objects/Camera.h
@@ -14,6 +14,15 @@ public:
     inline void SetPosition(Point pos) { m_Position = pos; }
     inline void Translate(Point translation) { m_Position += translation; }
 
+    inline void SetRotation(float angle) { m_RotationAngle = angle; SetMatrix(); }
+    inline void SetRotation(Rotation rot) { m_Rotation = rot; }
+
+private:
+    inline void SetMatrix() { m_Rotation = Rotation().FromEulerAngles(0, 0, m_RotationAngle); }
+
 private:
     Point m_Position;
+
+    float m_RotationAngle;
+    Rotation m_Rotation;
 };
